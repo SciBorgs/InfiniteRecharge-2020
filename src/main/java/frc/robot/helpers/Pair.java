99b980a01776eed685ef1610408bfb8045850cbc
Matthew <44071877+NastalgiaMeh@users.noreply.github.com>
2019-08-04 16:12:15 -0400

    Created pair class

---
+++ src/main/java/frc/robot/helpers/Pair.java
@@ -0,0 +1,14 @@
+package frc.robot.helpers;
+
+public class Pair<First, Second> {
+
+    public final First first;
+    public final Second second;
+  
+    public Pair(First first, Second second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+// :D
