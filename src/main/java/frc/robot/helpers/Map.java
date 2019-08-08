f668f78001e8b6285d5d71019bb15b91bf751d5b
zevbo <minsky-pz@bxscience.edu>
2019-08-08 13:25:43 -0400

    added Ray and Line functionality to WallMap interface (also Map renamed to WallMap)

--- src/main/java/frc/robot/helpers/Map.java
+++ src/main/java/frc/robot/helpers/Map.java
@@ -2,11 +2,15 @@ package frc.robot.helpers;
 
 import java.util.ArrayList;
 
-public interface Map{
+public interface WallMap{
 
-    // Returns an arraylist with all points on the map that intersect the given LineSegment
+    // Returns an arraylist with all points on the map that intersect the given LineSegment/Ray/Line
     ArrayList<Point> allIntersections(LineSegment ls); 
-
+    ArrayList<Point> allIntersections(Ray r); 
+    ArrayList<Point> allIntersections(Line l); 
+    
     // Determins wether or not a line segment intersects a place on the map
     boolean intersects(LineSegment ls);
+    boolean intersects(Ray r);
+    boolean intersects(Line l);
 }
---
+++ src/main/java/frc/robot/helpers/Ray.java
@@ -0,0 +1,9 @@
+package frc.robot.helpers;
+
+public class Ray{
+    public Point pEnd, pDir;
+    public Ray(Point pEnd, Point pDir){
+        this.pEnd = pEnd;
+        this.pDir = pDir;
+    }
+}
