32ec51f396c7e9d7bbee33bca0e4d2c419f69081
zevbo <minsky-pz@bxscience.edu>
2019-08-08 10:23:36 -0400

    updating name

--- src/main/java/frc/robot/helpers/Map.java
+++ src/main/java/frc/robot/helpers/Map.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public interface Map{
 
     // Returns an arraylist with all points on the map that intersect the given LineSegment
-    ArrayList<Point> mapIntersect(LineSegment ls); 
+    ArrayList<Point> allIntersections(LineSegment ls); 
 
     // Determins wether or not a line segment intersects a place on the map
     boolean intersects(LineSegment ls);
