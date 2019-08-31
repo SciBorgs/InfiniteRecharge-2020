497b60796f5340f34aa0bd03240d3e359eed8155
zevbo <minsky-pz@bxscience.edu>
2019-08-31 12:06:46 -0400

    coordinateSystemHelper now called geo, and added LineLike class + AlmostLine interface in order to make more general functions

--- src/main/java/frc/robot/helpers/LineSegment.java
+++ src/main/java/frc/robot/helpers/LineSegment.java
@@ -1,9 +1,21 @@
 package frc.robot.helpers;
 
-public class LineSegment{
+import frc.robot.Utils;
+
+public class LineSegment extends LineLike implements AlmostLine{
     public Point p1, p2;
     public LineSegment(Point p1, Point p2){
         this.p1 = p1;
         this.p2 = p2;
+    }
+
+    public boolean contains(Point p){
+        return this.toLine().contains(p) && 
+            Utils.inBounds(p.x, new Pair<>(p1.x, p2.x)) && 
+            Utils.inBounds(p.y, new Pair<>(p1.y, p2.y));
+    }
+
+    public Point[] getBounds(){
+        return new Point[]{p1, p2};
     }
 }
