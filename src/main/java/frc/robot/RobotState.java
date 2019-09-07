d1c398e38bc75b621dbd4b7e1e00326343c5b7d9
zevbo <minsky-pz@bxscience.edu>
2019-09-07 08:16:50 -0400

    more RobotState functionality

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -2,7 +2,9 @@ package frc.robot;
 
 import frc.robot.stateEstimation.*;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Set;
 
 public class RobotState {
     
@@ -19,12 +21,10 @@ public class RobotState {
         this.data = data;
     }
 
-    public double get(RS rs){
-        return this.data.get(rs);
-    }
-    public void set(RS rs, double val){
-        this.data.put(rs, val);
-    }
+    public double get(RS rs)      {return this.data.get(rs);}
+    public void   set(RS rs, double val) {this.data.put(rs, val);}
+    public void   remove(RS rs)          {this.data.remove(rs);}
+
     public RobotState copy(){
         return new RobotState((Hashtable<RS, Double>) data.clone());
     }
@@ -33,4 +33,20 @@ public class RobotState {
         newRobotState.set(rs, val);
         return newRobotState;
     }
+    public Set<RS> keys(){return data.keySet();}
+
+    public void incorporateOtherState(RobotState otherState){
+        for(RS rs : otherState.keys()){
+            set(rs, otherState.get(rs));
+        }
+    }
+
+    public void cutDownTo(ArrayList<RS> rsToInclude){
+        for(RS rs : keys()){
+            if (!rsToInclude.contains(rs)){
+                remove(rs);
+            }
+        }
+    }
+
 }
