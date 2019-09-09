e5791f7e51fdcb77238a2f1380e6bda12e1a2efd
zevbo <minsky-pz@bxscience.edu>
2019-09-09 15:23:02 -0400

    cut down on code duplication

--- src/main/java/frc/robot/Tester.java
+++ src/main/java/frc/robot/Tester.java
@@ -2,22 +2,23 @@ package frc.robot;
 
 public class Tester{
 
+    private static void testFailed(String testName, String description){
+        throw new RuntimeException("test \"" + testName + "\" FAILED: " + description);
+    }
+    private static void testBool(String testName, String description, boolean succeded){
+        if (!succeded){testFailed(testName, description);}
+    }
+
     public static void assertTrue(boolean b, String testName) {
-        if (!b) {
-            throw new RuntimeException("test \"" + testName + "\" FAILED: Statement false");
-        }
+        testBool(testName, "Statement false", b);
     }
     
     public static void assertEquals(Number n1, Number n2, String testName){
-        if (n1 != n2){
-            throw new RuntimeException("test \"" + testName + "\" FAILED: Assertion that " + n1 + " == " + n2);
-        }
+        testBool(testName, "Assertion that " + n1 + " == " + n2, n1 == n2);
     }
 
     public static void assertEquals(Object o1, Object o2, String testName){
-        if (!o1.equals(o2)){
-            throw new RuntimeException("test \"" + testName + "\" FAILED: Assertion that " + o1 + " equals " + o2);
-        }
+        testBool(testName, "Assertion that " + o1 + " equals " + o2, o1.equals(o2));
     }
 
 }
