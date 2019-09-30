a7eb1bc5992c10e84bc169ad06f1f374a32201dd
NastalgiaMeh <44071877+NastalgiaMeh@users.noreply.github.com>
2019-09-29 20:28:37 -0400

    Created more tests, especially for methods with "lLike" parameters.
    Can't seem to find any edge cases to put in.

--- src/main/java/frc/robot/tests/GeoTest.java
+++ src/main/java/frc/robot/tests/GeoTest.java
@@ -1,5 +1,8 @@
 package frc.robot.tests;
+
 import frc.robot.helpers.*;
+
+import java.util.Optional;
 
 public class GeoTest {
 
@@ -26,10 +29,9 @@ public class GeoTest {
         Tester.assertNotEquals(Geo.flipXandY(new Point(500,200)), new Point(500,200), "flipPoint, Test #2");
         
         /// Flip x and y (Line)
-        Tester.assertEquals(Geo.flipXandY(new Line(new Point(0,1), new Point(1,2))), 
-                                          new Line(new Point(1,0), new Point(2,1)), "flipLine, Test #1");
+        Line flipXYLine = new Line(new Point(0,1), new Point(1,2));
+        Tester.assertEquals(Geo.flipXandY(flipXYLine), new Line(new Point(1,0), new Point(2,1)), "flipLine, Test #1");
 
-    
         /// ThetaOf
         Line l = new Line(o, new Point(1, 0));
         Tester.assertEquals(Math.toDegrees(Geo.thetaOf(l)),          0, "thetaOf, Test #1");
@@ -73,19 +75,18 @@ public class GeoTest {
         /// Point slope form
         Tester.assertEquals(Geo.pointSlopeForm(new Point(0,-4), 3), new Line(new Point(0,-4), new Point(1,-1)),  "pointSlopeForm, Test #1");
         Tester.assertEquals(Geo.pointSlopeForm(new Point(1,4),  2), new Line(new Point(1,4),  new Point(2,6)),   "pointSlopeForm, Test #2");
-        Tester.assertEquals(Geo.pointSlopeForm(new Point(2,2),  0), new Line(new Point(2,2),  new Point(3,2)),   "pointSlopeForm, Test #3");
+        Tester.assertEquals(Geo.pointSlopeForm(new Point(2,2),  0), new Line(new Point(2,2),  new Point(3,2)),   "pointSlopeForm, Test #3"); // Horizontal
         
         /// Slope intercept form
         Tester.assertEquals(Geo.slopeInterceptForm(2,3),  new Line(new Point(0,3),  new Point(1,5)),  "slopeInterceptForm, Test #1");
         Tester.assertEquals(Geo.slopeInterceptForm(1,-3), new Line(new Point(0,-3), new Point(1,-2)), "slopeInterceptForm, Test #2");
-        Tester.assertEquals(Geo.slopeInterceptForm(0,2),  new Line(new Point(0,2),  new Point(1,2)),  "slopeInterceptForm, Test #3");
+        Tester.assertEquals(Geo.slopeInterceptForm(0,2),  new Line(new Point(0,2),  new Point(1,2)),  "slopeInterceptForm, Test #3"); // Horizontal
         
         /// Point Angle Form
         Tester.assertEquals(Geo.pointAngleForm(new Point(2,0), Math.toRadians(45)),  
                                                new Line(new Point(2,0),  new Point(3,1)),  "pointAngleForm, Test #1");
         Tester.assertEquals(Geo.pointAngleForm(new Point(2,0), Math.toRadians(135)), 
                                                new Line(new Point(2,0),  new Point(3,-1)), "pointAngleForm, Test #2");
-        
         Tester.assertEquals(Geo.pointAngleForm(o, Math.toRadians(90)),  
                                                new Line(o, new Point(0,1)), "pointAngleForm, Test #3");
         
@@ -94,21 +95,24 @@ public class GeoTest {
         Tester.assertNotEquals(Geo.getDistance(o, new Point(3,0 )), 1, "distanceOfTwoPoints, #2");
 
         /// Distance between lineLike and point 
-        Tester.assertEquals(Geo.getDistance(new Line(       new Point(2,0), new Point(2,2)), new Point(0,0)),  2, "distanceLinePoint");
+        Tester.assertEquals(Geo.getDistance(new Line(       new Point(2,0), new Point(2,2)), o),  2, "distanceLinePoint");
         Tester.assertEquals(Geo.getDistance(new LineSegment(new Point(0,1), new Point(1,0)), new Point(1,-1)), 1, "distanceLineSegmentPoint");
         Tester.assertEquals(Geo.getDistance(new Ray(        new Point(1,0), new Point(0,2)), new Point(1,-1)), 1, "distanceRayPoint");
 
         /// arePointsCollinear
         Tester.assertTrue( Geo.arePointsCollinear(new Point(1,0), new Point(1,2), new Point(1,1)), "arePointsCollinear, Test #1");
         Tester.assertTrue( Geo.arePointsCollinear(new Point(3,0), new Point(1,2), new Point(2,1)), "arePointsCollinear, Test #2");
+        Tester.assertTrue( Geo.arePointsCollinear(new Point(0,1), new Point(2,1), new Point(1,1)), "arePointsCollinear, Test #3");
+        Tester.assertTrue( Geo.arePointsCollinear(new Point(0,3), new Point(3,0), new Point(1,2)), "arePointsCollinear, Test #4");
         
-        Tester.assertFalse(Geo.arePointsCollinear(new Point(1,0), new Point(1,2), new Point(0,1)), "arePointsCollinear, Test #3");
-        Tester.assertFalse(Geo.arePointsCollinear(new Point(3,0), new Point(1,2), new Point(3,2)), "arePointsCollinear, Test #4");
+        Tester.assertFalse(Geo.arePointsCollinear(new Point(1,0), new Point(1,2), new Point(0,1)), "arePointsCollinear, Test #5");
+        Tester.assertFalse(Geo.arePointsCollinear(new Point(3,0), new Point(1,2), new Point(3,2)), "arePointsCollinear, Test #6");
         
         /// getMidpoint
         Tester.assertEquals(   Geo.getMidpoint(new Point(1,0), new Point(1,2)), new Point(1,1), "Midpoint, Test #1");
         Tester.assertEquals(   Geo.getMidpoint(new Point(1,0), new Point(3,0)), new Point(2,0), "Midpoint, Test #2");
         Tester.assertNotEquals(Geo.getMidpoint(new Point(1,0), new Point(3,0)), new Point(3,0), "Midpoint, Test #3");
+        Tester.assertNotEquals(Geo.getMidpoint(o, new Point(8,0)), new Point(2,0), "Midpoint, Test #4");
 
         /// getPerpendicular
         Tester.assertEquals(Geo.getPerpendicular(new Line(new Point(1,0),  new Point(1,2)), new Point(1,1)), 
@@ -126,22 +130,52 @@ public class GeoTest {
         /// areParallel
         Line parallelLine1 = new Line(new Point(0,2), new Point(-2,0));
         Line parallelLine2 = new Line(new Point(2,0), new Point(0,-2));
+
+        Line notParallelLine1 = new Line(o, new Point(1,1));
+        Line notParallelLine2 = new Line(new Point(1,0), new Point(0,1));
+
+        Ray parallelRay1 = new Ray(new Point(1,0),  new Point(0,1));
+        Ray parallelRay2 = new Ray(new Point(1,-1), o);
+
+        LineSegment parallelSegment1 = new LineSegment(new Point(2,2), new Point(2,0));
+        LineSegment parallelSegment2 = new LineSegment(new Point(1,0), new Point(1,2));
         
-        Tester.assertTrue(Geo.areParellel(new Line(new Point(0,0), new Point(0,1)), 
+        Tester.assertTrue(Geo.areParellel(new Line(o, new Point(0,1)), 
                                           new Line(new Point(1,0), new Point(1,1))), "areParallel, Test #1"); // Vertical
         
         Tester.assertTrue(Geo.areParellel(parallelLine1, parallelLine2), "areParallel, Test #2");
         Tester.assertTrue(Geo.areParellel(flipPoints(parallelLine1), flipPoints(parallelLine2)), "areParallel, Test #3");
 
+        Tester.assertTrue(Geo.areParellel(parallelRay1, parallelRay2), "areParallel, Test #4");
+
+        Tester.assertTrue(Geo.areParellel(parallelSegment1, parallelSegment2), "areParallel, Test #5");
+
+        Tester.assertFalse(Geo.areParellel(notParallelLine1, notParallelLine2), "areParallel, Test #6");
+
         /// getIntersection
         Line intersectionLine1 = new Line(new Point(-1,2), new Point(1,0));
         Line intersectionLine2 = new Line(new Point(-1,0), new Point(1,2));
 
+        Ray noIntersectionRay1 = new Ray(new Point(3,0), new Point(4,1));
+        Ray noIntersectionRay2 = new Ray(new Point(2,1), new Point(1,2));
+        
+        Ray yesIntersectionRay1 = new Ray(new Point(4,0), new Point(3,-1));
+        Ray yesIntersectionRay2 = new Ray(o, new Point(1,-1));
+
+        LineSegment intersectionSegment1 = new LineSegment(new Point(3,0), new Point(1,2));
+        LineSegment intersectionSegment2 = new LineSegment(new Point(1,0), new Point(2,1));
+
         Tester.assertEquals(Geo.getIntersection(new Line(new Point(0,1), new Point(1,0)), 
-                                                new Line(new Point(0,0), new Point(1,1))).get(), new Point(0.5,0.5), "getIntersection, Test #1");
+                                                new Line(o, new Point(1,1))).get(), new Point(0.5,0.5), "getIntersection, Test #1");
         
         Tester.assertEquals(Geo.getIntersection(intersectionLine1, intersectionLine2).get(), new Point(0,1), "getIntersection, Test #2");
-        Tester.assertEquals(Geo.getIntersection(flipPoints(intersectionLine1), flipPoints(intersectionLine2)).get(), new Point(0,1), "getIntersection, Test #3");
+        Tester.assertEquals(Geo.getIntersection(flipPoints(intersectionLine1), 
+                                                flipPoints(intersectionLine2)).get(), new Point(0,1), "getIntersection, Test #3");
+        
+        Tester.assertEquals(Geo.getIntersection(noIntersectionRay1, noIntersectionRay2), Optional.empty(), "getIntersection, Test #4");
+        Tester.assertEquals(Geo.getIntersection(yesIntersectionRay1, yesIntersectionRay2).get(), new Point(2,-2), "getIntersection, Test #5");
+
+        Tester.assertEquals(Geo.getIntersection(intersectionSegment1, intersectionSegment2).get(), new Point(2,1), "getIntersection, Test #6");
 
         /// Scale point
         Tester.assertEquals(Geo.scale(new Point(4,2), 3),   new Point(12,6),   "scale, Test #1");
@@ -155,7 +189,7 @@ public class GeoTest {
         /// Subtract points
         Tester.assertEquals(Geo.sub(new Point(1,1), new Point(1,1)),              o, "sub, Test #1");
         Tester.assertEquals(Geo.sub(new Point(5,2), new Point(1,1)), new Point(4,1), "sub, Test #2");
-        Tester.assertEquals(Geo.sub(new Point(1,1), new Point(1,1)), new Point(0,0), "sub, Test #3");
+        Tester.assertNotEquals(Geo.sub(new Point(1,1), new Point(1,1)), new Point(2,2), "sub, Test #3"); // Confused with add and sub
 
         /// getMagnitude
         Tester.assertEquals(Geo.getMagnitude(new Point(2,0)),  2, "getMagnitude, Test #1");
