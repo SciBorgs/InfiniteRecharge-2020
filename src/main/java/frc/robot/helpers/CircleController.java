763799a1a110ba0a88997859df2de773acd5c05e
swan.goose.duck <kaneres@bxscience.edu>
2019-09-30 17:46:25 -0400

    Change algo for circleController

--- src/main/java/frc/robot/helpers/CircleController.java
+++ src/main/java/frc/robot/helpers/CircleController.java
@@ -4,35 +4,27 @@ import frc.robot.Robot;
 
 public class CircleController { 
 
-    private static final double ROBOT_WIDTH = 0;
-    private static final double ANGLE_P = 0;
-    private static final double POS_P = 0;
-    private static final double TURN_P = 0;
-    private PID anglePID = new PID(ANGLE_P, 0, 0);
-    private PID posPID = new PID(POS_P, 0, 0);
-    private PID turnPID = new PID(TURN_P, 0, 0);
+    private static final double FINAL_HEADING_P = 0;
+    private static final double DESIRED_HEADING_P = 0;
+    private PID finalHeadingPID   = new PID(FINAL_HEADING_P, 0, 0);
+    private PID desiredHeadingPID = new PID(DESIRED_HEADING_P, 0, 0);
 
     public CircleController () {}
 
     public void update (Point currPos, double currHeading, Point finalPos, double finalHeading) {
         Circle currCircle  = Circle.makeCircleWithTangentAnd2Points(currPos, currHeading, finalPos);
-        
-        double curvature = 1 / currCircle.radius;
-        double targetTurnMagnitude = 1 + (ROBOT_WIDTH * curvature) / 2;
+        Line finalHeadingLine = Geo.pointAngleForm(finalPos, finalHeading);
+        double mFinalHeadingLine = Geo.mOf(finalHeadingLine);
+        Line tangentOnCircleToFinalPos = Geo.pointSlopeForm(finalPos, Geo.getTangentToCircle(currCircle, finalPos));
+        double mTangentOnCircleToFinalPos = Geo.mOf(tangentOnCircleToFinalPos);
 
-        double errorPos   = Geo.getMagnitude(Geo.sub(currPos, finalPos));
-        double errorAngle = finalHeading - currHeading;
-        anglePID.addMeasurement(errorAngle);
-        posPID.addMeasurement(errorPos);
-        double turnMagnitude = anglePID.getOutput() + posPID.getOutput();
-        
-        double errorTurn = targetTurnMagnitude - turnMagnitude;
-        turnPID.addMeasurement(errorTurn);
-        
-        Robot.driveSubsystem.setSpeedTankTurningPercentage(turnPID.getOutput());
+        double finalHeadingError = Math.atan((mFinalHeadingLine - mTangentOnCircleToFinalPos) / (1 - mFinalHeadingLine * mTangentOnCircleToFinalPos));
+        finalHeadingPID.addMeasurement(finalHeadingError);
+
+        double desiredHeadingError = finalHeading - currHeading;
+        desiredHeadingPID.addMeasurement(desiredHeadingError);
+
+        double turnMagnitude = desiredHeadingPID.getOutput() + finalHeadingPID.getOutput();           
+        Robot.driveSubsystem.setSpeedTankTurningPercentage(turnMagnitude);
     }
-
-
-
-
 }
