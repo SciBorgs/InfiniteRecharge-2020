7f5fd1c329fee29d1ba6170bf2cd239d9d206738
NastalgiaMeh <44071877+NastalgiaMeh@users.noreply.github.com>
2019-10-07 19:16:19 -0400

    Fixed Geo. Did not change any tests. Every method (excluding Circles) so far should be good.

---
+++ src/main/java/frc/robot/shapes/Circle.java
@@ -0,0 +1,38 @@
+package frc.robot.shapes;
+
+import frc.robot.helpers.Geo;
+
+public class Circle {
+    
+    public Point center;
+    public double radius;
+
+    public Circle (Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public static Circle twoPointTangentAngleForm (Point currPos, double currHeading, Point finalPos) {
+        // rotate currPos so that currHeading becomes 0
+        Point currPosRotated  = Geo.rotatePoint(currPos,  -1 * currHeading);
+        Point finalPosRotated = Geo.rotatePoint(finalPos, -1 * currHeading);
+
+        double kRotated = calculateK(currPosRotated, finalPosRotated);
+        double hRotated = currPosRotated.x;
+        double radius   = Math.abs(currPosRotated.y - kRotated);
+
+        Point center = Geo.rotatePoint(new Point(hRotated, kRotated), currHeading);
+        return new Circle(center, radius);
+    }
+
+    private static double calculateK (Point currPosRotated, Point finalPosRotated) {
+        double x0, y0, x1, y1;
+        x0 = currPosRotated.x;
+        y0 = currPosRotated.y;
+        x1 = finalPosRotated.x;
+        y1 = finalPosRotated.y;
+        double a = (y1 * y1) - (y0 * y0) + ((x1 - x0) * (x1 - x0));
+        double b = 2 * (y1 - y0);
+        return a / b;
+    }
+}
