85e080eda9c15e548e0e0ed495ee47b3ab9dd34a
Erwln <sirpythagoras@gmail.com>
2019-10-20 20:20:43 -0400

    Tested implementation of Deque

--- src/main/java/frc/robot/RobotStateHistory.java
+++ src/main/java/frc/robot/RobotStateHistory.java
@@ -1,47 +1,48 @@
 package frc.robot;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ListIterator;
 
 import frc.robot.RobotState;
-import frc.robot.helpers.QueuedArrayList;
+import frc.robot.helpers.Deque;
 
 public class RobotStateHistory{
 
     // allows us to more clearly pass through a history of states
     // and really allows it to be distinguished from just a list of RobotState objects
 
-    private QueuedArrayList<RobotState> robotStates;
+    private Deque<RobotState> robotStates;
     public int DEFAULT_MAX_SIZE = 30;
 
     public RobotStateHistory() {
-        this.robotStates = new QueuedArrayList<RobotState>(DEFAULT_MAX_SIZE);
+        this.robotStates = new Deque<RobotState>(DEFAULT_MAX_SIZE);
         this.robotStates.add(new RobotState());
     }
     public RobotStateHistory(int maxLength){
-        this.robotStates = new QueuedArrayList<RobotState>(maxLength);
+        this.robotStates = new Deque<RobotState>(maxLength);
         this.robotStates.add(new RobotState());
     }
-    public RobotStateHistory(QueuedArrayList<RobotState> robotStates){
+    public RobotStateHistory(Deque<RobotState> robotStates){
         this.robotStates = robotStates;
     }
     public RobotStateHistory(Iterable<RobotState> robotStates){
-        this.robotStates = QueuedArrayList.fromIterable(robotStates, DEFAULT_MAX_SIZE);
+        this.robotStates = new Deque<>(robotStates, DEFAULT_MAX_SIZE);
     }
     public RobotStateHistory(Iterable<RobotState> robotStates, int maxLength){
-        this.robotStates = QueuedArrayList.fromIterable(robotStates, maxLength);
+        this.robotStates = new Deque<>(robotStates, maxLength);
     }
 
     public RobotState currentState(){return statesAgo(0);}
-    public QueuedArrayList<RobotState> getQueuedArrayList(){return this.robotStates;}
-    public ArrayList<RobotState> getArrayList(){return this.robotStates.toArrayList();}
+    public Deque<RobotState> getQueuedArrayList(){return this.robotStates;}
+    public List<RobotState> getArrayList(){return this.robotStates.getArrayList();}
     public RobotState statesAgo(int n)    {return this.robotStates.get(n);}
-    public int numberOfStates()           {return this.robotStates.size();}
+    public int numberOfStates()           {return this.robotStates.getSize();}
     public void setCurrentState(RobotState state){this.robotStates.set(0, state);}
     public void addState       (RobotState state){this.robotStates.add(state);}
     public void dropFirstState()                 {this.robotStates.remove(0);}
     
     public RobotStateHistory copy(){
-        return new RobotStateHistory((QueuedArrayList<RobotState>) this.robotStates.clone());
+        return new RobotStateHistory((Deque<RobotState>) this.robotStates.clone());
     }
 }
