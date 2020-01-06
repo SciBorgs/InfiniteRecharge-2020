a7daa3ceb71fb608972ab755573e8d1ec2aa0fbf
redpossibl <buenas@bxscience.edu>
2020-01-06 17:17:09 -0500

    climberController

--- src/main/java/frc/robot/controllers/ClimberController.java
+++ src/main/java/frc/robot/controllers/ClimberController.java
@@ -3,25 +3,33 @@ package frc.robot.controllers;
 import frc.robot.controllers.PID;
 import frc.robot.robotState.RobotState.SD;
 import frc.robot.Robot;
+import frc.robot.PortMap;
+
 public class ClimberController {
-    private double MAXIMUM_TILT_ANGLE = Math.toRadians(14.5);
-    private double reachP = 1;
+    private final double MAXIMUM_TILT_ANGLE = Math.toRadians(14.5);
+    private final double reachP = 1;
+    private final double tiltP  = 1/MAXIMUM_TILT_ANGLE;
+    private double tiltOffset   = 0;
+
     private PID reachPID;
-    private double tiltP = 1/MAXIMUM_TILT_ANGLE;
     private PID tiltPID;
-    private double tiltOffset = 0;
-    public ClimberController(){
+
+    public ClimberController() {
         reachPID = new PID(reachP, 0, 0);
-        tiltPID = new PID(tiltP, 0, 0);
-        
+        tiltPID  = new PID(tiltP, 0, 0);
     }
 
-    public void beforeReach(){
+    public void beforeReach() {
         tiltOffset = Robot.get(SD.TiltAngle);
     }
 
-    public void moveToHeight(double height){
-        reachPID.addMeasurement(height-Robot.get(SD.ClimberHeight));
+    public void moveToHeight(double height) {
+        reachPID.addMeasurement(height - Robot.get(SD.ClimberHeight));
         Robot.climberSubsystem.setLiftSpeed(reachPID.getOutput());
     }
+
+    /*public void moveToAngle(double angle) {
+        tiltPID.addMeasurement(angle - Robot.get(SD.TiltAngle));
+        Robot.climberSubsystem.setLiftSpeed(tiltPID.getOutput())
+    } No */
 }
