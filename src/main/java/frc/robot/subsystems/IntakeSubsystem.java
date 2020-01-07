8ea4e4c65fbad8e3dada1c48065b964adbe49de5
Sakib Pathen <37029585+todalta@users.noreply.github.com>
2020-01-07 16:24:32 -0500

    Added and fixed on stuff based on code review

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -5,7 +5,6 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import frc.robot.robotState.RobotState.SD;
 import frc.robot.PortMap;
 import frc.robot.Utils;
 
@@ -13,41 +12,35 @@ public class IntakeSubsystem extends Subsystem {
     DoubleSolenoid upDownSolenoid;
     TalonSRX intakeMotor;
 
-    private final DoubleSolenoid.Value OPEN_VALUE   = Value.kForward;
-    private final DoubleSolenoid.Value CLOSED_VALUE = Utils.oppositeDoubleSolenoidValue(OPEN_VALUE);
-    public  boolean AcceptingCell;
-    public final double INTAKE_SPEED = 0.01; //Temporary
+    private static final DoubleSolenoid.Value OPEN_VALUE   = Value.kForward;
+    private static final DoubleSolenoid.Value CLOSED_VALUE = Utils.oppositeDoubleSolenoidValue(OPEN_VALUE);
+    public  boolean acceptingCell;
+    public static final double INTAKE_SPEED = 0.01; //Temporary
 
     public IntakeSubsystem() {
         this.upDownSolenoid = new DoubleSolenoid(PortMap.INTAKE_SOLENOID_FORWARD, PortMap.INTAKE_SOLENOID_REVERSE);
         this.intakeMotor = new TalonSRX(PortMap.INTAKE_TALON);
-        AcceptingCell = true;
+        this.acceptingCell = true;
     }
-    void SetIntakeSpeed(final double speed) {
+    public void setIntakeSpeed(final double speed) {
         Robot.driveSubsystem.setMotorSpeed(this.intakeMotor, speed);
     }
 
-    void closeIntake()        {this.upDownSolenoid.set(CLOSED_VALUE);}
-    void openIntake()         {this.upDownSolenoid.set(OPEN_VALUE);}
-    int  getEnergyBallCount() {return (int)Robot.get(SD.EnergyBallCount);}
-    boolean getAcceptingCell(){return getEnergyBallCount() < 5;}
-    void setAcceptingCell()   {this.AcceptingCell = getAcceptingCell();}
-    void addBallCount()       {Robot.set(SD.EnergyBallCount, getEnergyBallCount()+1);}
+    public void closeIntake() {this.upDownSolenoid.set(CLOSED_VALUE);}
+    public void openIntake() {this.upDownSolenoid.set(OPEN_VALUE);}
 
-    void takeInCell() {
-        if (this.AcceptingCell) {
-            closeIntake();
-            SetIntakeSpeed(this.INTAKE_SPEED);
-            addBallCount();
-        } else {
-            closeIntake();
-            SetIntakeSpeed(0.0);
-        }
+    public void suck() {
+        if (this.acceptingCell) {setIntakeSpeed(INTAKE_SPEED);} 
+        else {stop();}
+    }
+
+    public void stop() {
+        setIntakeSpeed(0.0);
     }
 
     @Override 
     protected void initDefaultCommand() {
-
+        // not needed
     }
 
 }
