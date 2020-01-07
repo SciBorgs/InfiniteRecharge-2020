99b7abf6e09cbdc2f31864b00e9ae3ec0b8a9faa
oliver-ruc <57429193+oliver-ruc@users.noreply.github.com>
2020-01-07 16:28:30 -0500

    Added movetoheight and movetobalance in climbercontroller

--- src/main/java/frc/robot/controllers/ClimberController.java
+++ src/main/java/frc/robot/controllers/ClimberController.java
@@ -3,33 +3,43 @@ package frc.robot.controllers;
 import frc.robot.controllers.PID;
 import frc.robot.robotState.RobotState.SD;
 import frc.robot.Robot;
-import frc.robot.PortMap;
-
 public class ClimberController {
-    private final double MAXIMUM_TILT_ANGLE = Math.toRadians(14.5);
-    private final double reachP = 1;
-    private final double tiltP  = 1/MAXIMUM_TILT_ANGLE;
-    private double tiltOffset   = 0;
-
+    private double MAXIMUM_TILT_ANGLE = Math.toRadians(14.5);
+    private double turnP = 1/(Math.PI/2);
+    private PID turnPID;
+    private double reachP = 1;
     private PID reachPID;
+    private double tiltP = 1/MAXIMUM_TILT_ANGLE;
     private PID tiltPID;
+    private double tiltOffset = 0;
 
     public ClimberController() {
-        reachPID = new PID(reachP, 0, 0);
-        tiltPID  = new PID(tiltP, 0, 0);
+        this.turnPID  = new PID(this.turnP,  0, 0);
+        this.reachPID = new PID(this.reachP, 0, 0);
+        this.tiltPID  = new PID(this.tiltP,  0, 0);
     }
 
-    public void beforeReach() {
-        tiltOffset = Robot.get(SD.TiltAngle);
+    public void adjustBeforeReach() { // for better lining up with the bar
+        double angleGoal = Math.toRadians(67.5); // turing towards red trench
+        if (Math.abs(angleGoal - Robot.get(SD.Angle)) > Math.PI/2) { // if the robot is angled closer to the blue trench then the red trench for grabbing the bar
+            angleGoal -= Math.PI; // set the angle goal towards the blue trench
+        }
+        this.turnPID.addMeasurement(angleGoal - Robot.get(SD.Angle));
+    }
+
+    public void beforeReach() { // for the sensors to be adjusted/ offset to be measured
+        this.tiltOffset = Robot.get(SD.TiltAngle);
     }
 
     public void moveToHeight(double height) {
-        reachPID.addMeasurement(height - Robot.get(SD.ClimberHeight));
-        Robot.climberSubsystem.setLiftSpeed(reachPID.getOutput());
+        this.reachPID.addMeasurement(height-Robot.get(SD.ClimberHeight));
+        Robot.climberSubsystem.setLiftSpeed(this.reachPID.getOutput());
     }
 
-    /*public void moveToAngle(double angle) {
-        tiltPID.addMeasurement(angle - Robot.get(SD.TiltAngle));
-        Robot.climberSubsystem.setLiftSpeed(tiltPID.getOutput())
-    } No */
+    public void moveToBalance() {
+        // always going to want to move up the rod
+        this.tiltPID.addMeasurement(0 - Robot.get(SD.TiltAngle));
+        Robot.climberSubsystem.setShiftMotorSpeed(this.tiltPID.getOutput());
+        
+    }
 }
