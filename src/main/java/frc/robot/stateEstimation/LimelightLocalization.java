38f3d61849f4d567782a4faad2dc3d2152d1e364
toyotaghost <tobiasalam@gmail.com>
2020-01-10 15:58:35 -0500

    fixing getting target angle

--- src/main/java/frc/robot/stateEstimation/LimelightLocalization.java
+++ src/main/java/frc/robot/stateEstimation/LimelightLocalization.java
@@ -9,12 +9,12 @@ public class LimelightLocalization {
     public double radialDistanceFromLoadingBay = 5;
     public double radialDistanceFromInnerPort = 5;
 
-    public static final double CAMERA_ABOVE_GROUND_HEIGHT = 5;
-    public static final double CAMERA_HORIZONTAL_POV = 54 * Math.PI/180;;
-    public static final double CAMERA_VERTICAL_POV = 41 * Math.PI/180;
-    public static final double CAMERA_MOUNTING_ANGLE = 5;
-    public static final double INNER_PORT_HEIGHT = Utils.inchesToMeters(98.25);
-    public static final double LOADING_BAY_HEIGHT = Utils.inchesToMeters(11);
+    private static final double CAMERA_ABOVE_GROUND_HEIGHT = 5;
+    private static final double CAMERA_HORIZONTAL_POV = 54 * Math.PI/180;;
+    private static final double CAMERA_VERTICAL_POV = 41 * Math.PI/180;
+    private static final double CAMERA_MOUNTING_ANGLE = 5;
+    private static final double INNER_PORT_HEIGHT = Utils.inchesToMeters(98.25);
+    private static final double LOADING_BAY_HEIGHT = Utils.inchesToMeters(11);
 
     public LimelightLocalization(LimelightSubsystem limeLight) {
         this.limeLight = limeLight;
@@ -31,14 +31,13 @@ public class LimelightLocalization {
     }
 
     public Point returnRobotPosition(double r, double theta, Point landmarkLocation) {
-        double xRelative = r * Math.cos(theta);
-        double yRelative = r * Math.sin(theta);
-        double xAbsolute = xRelative + landmarkLocation.x;
-        double yAbsolute = yRelative + landmarkLocation.y;
+        Point relativePoint = convertToCartesian(r, theta);
+        double xAbsolute = relativePoint.x + landmarkLocation.x;
+        double yAbsolute = relativePoint.y + landmarkLocation.y;
         return new Point(xAbsolute,yAbsolute);
     }
 
-    public double getYAngleToTarget(double pixelY) {
+    public double getYAngleToPixel(double pixelY) {
         double normalizedY = (1/120) * (119.5 - pixelY);
         double viewPlaneHeight = 2 * Math.tan(CAMERA_VERTICAL_POV/2);
         double y = viewPlaneHeight/2 * normalizedY;
@@ -49,8 +48,7 @@ public class LimelightLocalization {
     public boolean isInRange() { // for now, for simplicity, we will be assuming that the targets we find belong to the inner port
         if (limeLight.getTableData(limeLight.getCameraTable(), "getpipe") == 1) {
             if (limeLight.getTableData(limeLight.getCameraTable(), "tv") == 1) {
-                double pixelY = limeLight.getTableData(limeLight.getCameraTable(), "ty");
-                double yAngle = getYAngleToTarget(pixelY);                                   
+                double yAngle = limeLight.getTableData(limeLight.getCameraTable(), "ty");
                 double distance = calculateDistance(CAMERA_ABOVE_GROUND_HEIGHT, INNER_PORT_HEIGHT, CAMERA_MOUNTING_ANGLE, yAngle);
                 System.out.println("Target found");
                 return (distance <= radialDistanceFromInnerPort);           
