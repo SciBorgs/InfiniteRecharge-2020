9b86bf684273a92dc231e2328c480fb8887c5723
EllangoK <ellangok@bxscience.edu>
2020-01-11 10:29:30 -0500

    Create method to log spark encoder

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import frc.robot.PortMap;
 import frc.robot.Robot;
 import frc.robot.controllers.PID;
+import frc.robot.helpers.DelayedPrinter;
 import frc.robot.robotState.RobotState.SD;
 import frc.robot.sciSensorsActuators.SciSpark;
 import frc.robot.sciSensorsActuators.SciTalon;
@@ -11,6 +12,8 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class ShooterSubsystem extends Subsystem {
+  private SciSpark testSpark;
+
   private SciTalon rotateTalon;
   private SciSpark shooterMotorTop;
   private SciSpark shooterMotorBottom;
@@ -27,12 +30,18 @@ public class ShooterSubsystem extends Subsystem {
   private final int SHOOTER_D = 0;
 
   public ShooterSubsystem() {
+    this.testSpark = new SciSpark(0);
+
     this.rotateTalon  = new SciTalon(PortMap.SHOOTER_ROTATE_SPARK);
     this.shooterMotorTop = new SciSpark(PortMap.SHOOTER_MOTOR_TOP);
     this.shooterMotorBottom = new SciSpark(PortMap.SHOOTER_MOTOR_BOTTOM);
     this.limitSwitchLeft  = new DigitalInput(PortMap.SHOOTER_LEFT_DIGITAL_INPUT);
     this.limitSwitchRight = new DigitalInput(PortMap.SHOOTER_RIGHT_DIGITAL_INPUT);
     this.shooterPID = new PID(SHOOTER_P, SHOOTER_I, SHOOTER_D);
+  }
+
+  public void logTestSparkEncoder() {
+    DelayedPrinter.print("WHEEL ANGLE: " + this.testSpark.getWheelAngle());
   }
 
   public void setAngle(double angle) {
