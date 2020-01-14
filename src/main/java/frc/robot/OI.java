c82787bb4113dece27f9b9ffcdc1c5d06bc8db85
redpossibl <buenas@bxscience.edu>
2020-01-14 17:27:04 -0500

    fixed buttons

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -17,11 +17,12 @@ public class OI {
         xboxController = new XboxController(PortMap.XBOX_CONTROLLER);
 
         adjustTiltButton     = new JoystickButton(rightStick, PortMap.JOYSTICK_TRIGGER);
-        raiseTelescopeButton = new JoystickButton(rightStick, PortMap.JOYSTICK_TRIGGER); // change
-        pullStringButton     = new JoystickButton(rightStick, PortMap.JOYSTICK_TRIGGER); // change
-
         adjustTiltButton.whileHeld(new JoystickShiftCommand());
+
+        raiseTelescopeButton = new JoystickButton(rightStick, PortMap.JOYSTICK_LEFT_BUTTON); // change
         raiseTelescopeButton.whenPressed(new RaiseTelescopeCommand());
+
+        pullStringButton     = new JoystickButton(rightStick, PortMap.JOYSTICK_RIGHT_BUTTON); // change
         pullStringButton.whenPressed(new OnRungCommand());
     }
 }
