807103006e8d6e53d03ec477e0447b2d5d212aaf
redpossibl <buenas@bxscience.edu>
2020-01-25 16:26:03 -0500

    purged get()

--- src/main/java/frc/robot/sciSensorsActuators/SciSolenoid.java
+++ src/main/java/frc/robot/sciSensorsActuators/SciSolenoid.java
@@ -3,37 +3,33 @@ package frc.robot.sciSensorsActuators;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import frc.robot.Utils;
+import frc.robot.dataTypes.BiHashMap;
 
 public class SciSolenoid <ValueType extends Enum> extends DoubleSolenoid {
-    private ValueType forwardValue, backwardValue, offValue;
+    private BiHashMap<Value, ValueType> valueMap;
 
-    public SciSolenoid(int[] ports, ValueType forward, ValueType backward, ValueType off) {
-        this(1, ports, forward, backward, off);
+    public SciSolenoid(int[] ports, ValueType forwardValue, ValueType backwardValue, ValueType off) {
+        this(1, ports, forwardValue, backwardValue, off);
     }
 
-    public SciSolenoid(int pdpPort, int[] ports, ValueType forwardValue, ValueType backwardValue, ValueType offValue) {
+    public SciSolenoid(int pdpPort, int[] ports, ValueType forwardValue, ValueType reverseValue, ValueType offValue) {
         super(pdpPort, ports[0], ports[1]);
-        this.forwardValue  = forwardValue;
-        this.backwardValue = backwardValue;
-        this.offValue      = offValue;
+        this.valueMap = new BiHashMap<Value, ValueType>();
+        this.valueMap.put(Value.kForward, forwardValue);
+        this.valueMap.put(Value.kReverse, reverseValue);
+        this.valueMap.put(Value.kOff,     offValue);
     }
     
     private Value toDoubleSolenoidValue(ValueType e) {
-        if(e.equals(this.forwardValue))  {return Value.kForward;}
-        if(e.equals(this.backwardValue)) {return Value.kReverse;}
-        return Value.kOff;
+        return valueMap.getBackward(e);
     }
-    private ValueType toValueType(Value v){
-        if(v.equals(Value.kForward)) {return this.forwardValue;}
-        if(v.equals(Value.kReverse)) {return this.backwardValue;}
-        return this.offValue;
 
+    private ValueType toValueType(Value v){
+        return valueMap.getForward(v);
     }
 
     public ValueType oppositeSciSolenoidValue(ValueType e) {
-        if(e.equals(this.forwardValue))  {return this.backwardValue;}
-        if(e.equals(this.backwardValue)) {return this.forwardValue;}
-        return null;
+        return valueMap.getForward(Utils.oppositeDoubleSolenoidValue(valueMap.getBackward(e)));
     }
 
     public void set(ValueType e) {
@@ -41,7 +37,12 @@ public class SciSolenoid <ValueType extends Enum> extends DoubleSolenoid {
     }
 
     public void toggle() {
-        this.set(oppositeSciSolenoidValue(getValue()));
+        super.set(Utils.oppositeDoubleSolenoidValue(super.get()));
+    }
+
+    @Override @Deprecated
+    public Value get(){
+        throw new RuntimeException("get() is deprecated for SciSolenoids. Use getValue() instead");
     }
 
     public ValueType getValue() {return toValueType(super.get());}
