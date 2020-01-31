6e8b1a77ff7767b65ea8e88952f089a50f138709
Dhinak G <dhinakg@users.noreply.github.com>
2020-01-31 08:10:08 -0500

    Formatting tweaks

--- src/main/java/frc/robot/sciSensorsActuators/SciServo.java
+++ src/main/java/frc/robot/sciSensorsActuators/SciServo.java
@@ -7,24 +7,18 @@ public class SciServo extends Servo {
     private double minAngle;
 
     public SciServo(int channel) {
-	super(channel);
+        super(channel);
     }
 
     public double getMinAngle()    { return this.minAngle; }
     public double getCenterAngle() { return this.minAngle + ANGLE_RANGE/2; }
     public double getMaxAngle()    { return this.minAngle + ANGLE_RANGE; }
 
-    public void setMinAngle(double angle) {
-        this.minAngle = angle;
-    }
+    public void setMinAngle(double angle)    { this.minAngle = angle; }
 
-    public void setCenterAngle(double angle) {
-        this.minAngle = angle - ANGLE_RANGE/2.0;
-    }
+    public void setCenterAngle(double angle) { this.minAngle = angle - ANGLE_RANGE/2.0; }
 
-    public void setMaxAngle(double angle) {
-        this.minAngle = angle - ANGLE_RANGE;
-    }
+    public void setMaxAngle(double angle)    { this.minAngle = angle - ANGLE_RANGE; }
 
     @Override
     public double getAngle() {
