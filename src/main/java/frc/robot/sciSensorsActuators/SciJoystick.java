2149bb4f9b8baa1e7661dfe5eedb0d4cb481573e
RookieTEC9 <jeremiur.ps101q@gmail.com>
2020-01-31 15:39:39 -0500

    Added DeadZone for X/Y, deleted constructor

--- src/main/java/frc/robot/sciSensorsActuators/SciJoystick.java
+++ src/main/java/frc/robot/sciSensorsActuators/SciJoystick.java
@@ -4,22 +4,21 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public class SciJoystick extends Joystick {
 
-    private double inputDeadZone = 0.11; //deadzone because the joysticks are bad and they detect input when there is none
+    private double inputDeadZoneX = 0.11; //deadzone because the joysticks are bad and they detect input when there is none
+    private double inputDeadZoneY = 0.11; //deadzone because the joysticks are bad and they detect input when there is none
 
     public SciJoystick(int port) {super(port);}
-    public SciJoystick(int port, double inputDeadZone) {        
-        this(port);
-        this.inputDeadZone = inputDeadZone;
-    }
 
-    public void setInputDeadZone(double inputDeadZone) {this.inputDeadZone = inputDeadZone; }
-    public double getInputDeadZone() {return this.inputDeadZone;}
+    public void setInputDeadZoneX(double inputDeadZoneX) {this.inputDeadZoneX = inputDeadZoneX; }
+    public void setInputDeadZoneY(double inputDeadZoneY) {this.inputDeadZoneY = inputDeadZoneY; }
 
-    public double deadzone(double output) {
-        return Math.abs(output) < this.inputDeadZone ? 0 : output;
-    }
+    public double getInputDeadZoneX() {return this.inputDeadZoneX;}
+    public double getInputDeadZoneY() {return this.inputDeadZoneY;}
 
+    public double deadzoneX(double output) { return Math.abs(output) < this.inputDeadZoneX ? 0 : output; }
+    public double deadzoneY(double output) { return Math.abs(output) < this.inputDeadZoneY ? 0 : output; }
+    
     //Cannot override final method 
-    public double getProcessedX() {return  this.deadzone(this.getX());}
+    public double getProcessedX() {return -this.deadzone(this.getX());}
     public double getProcessedY() {return -this.deadzone(this.getY());}    
 }
