f77d261da291e598fb4b3976def0bd9b2bf2d967
Sakib Pathen <37029585+todalta@users.noreply.github.com>
2020-02-01 09:43:42 -0500

    Getting rid of codd where OI calls commands

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -9,18 +9,11 @@ import frc.robot.commands.*;
 public class OI {
     public Joystick leftStick, rightStick;
     public XboxController xboxController;
-    public JoystickButton intakeStartButton;
-    public JoystickButton intakeStopButton;
     
     public OI() {
         this.leftStick = new Joystick(PortMap.JOYSTICK_LEFT);
         this.rightStick = new Joystick(PortMap.JOYSTICK_RIGHT);
         this.xboxController = new XboxController(PortMap.XBOX_CONTROLLER);
-
-        this.intakeStartButton = new JoystickButton(rightStick, PortMap.JOYSTICK_LEFT_BUTTON); // Temporary
-        this.intakeStartButton.whenPressed(new IntakeSuckCommand());
-        this.intakeStopButton = new JoystickButton(rightStick, PortMap.JOYSTICK_RIGHT_BUTTON);
-        this.intakeStopButton.whenPressed(new IntakeReleaseCommand());
     }
 }
 
