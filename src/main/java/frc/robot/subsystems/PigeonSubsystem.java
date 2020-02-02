02a890360d851adb53ae0682f99098f8e363e327
zevbo <minsky-pz@bxscience.edu>
2020-02-02 15:46:55 -0500

    pigeon subsystem now uses the new sciUpdating shit. gang gang bois

--- src/main/java/frc/robot/subsystems/PigeonSubsystem.java
+++ src/main/java/frc/robot/subsystems/PigeonSubsystem.java
@@ -8,7 +8,7 @@ import frc.robot.sciSensorsActuators.SciPigeon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-public class PigeonSubsystem extends Subsystem implements RobotStateUpdater {
+public class PigeonSubsystem extends Subsystem {
   // for the main pigeon on the robot
     public  SciPigeon pigeon;
     private TalonSRX pigeonTalon;
@@ -16,13 +16,10 @@ public class PigeonSubsystem extends Subsystem implements RobotStateUpdater {
     public PigeonSubsystem () {
         this.pigeonTalon = new TalonSRX(PortMap.PIGEON_TALON);
         this.pigeon      = new SciPigeon(pigeonTalon);
-        Robot.addRobotStateUpdater(this);
+        this.pigeon.assignAngleSD(SD.PigeonAngle);
     }
 
     public SciPigeon getPigeon() {return this.pigeon;}
-    public void setAngle() {Robot.set(SD.PigeonAngle, 1*getPigeon().getAngle());}
-    @Override
-    public void updateRobotState(){setAngle();}
 
     @Override
     protected void initDefaultCommand() {
