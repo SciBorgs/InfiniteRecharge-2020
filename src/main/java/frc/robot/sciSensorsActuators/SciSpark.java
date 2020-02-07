7fdf10f9b379f354ae3f549a30e3eff6e02c892a
swan-goose-duck <kaneres@bxscience>
2020-02-07 16:43:02 -0500

    clean up

--- src/main/java/frc/robot/sciSensorsActuators/SciSpark.java
+++ src/main/java/frc/robot/sciSensorsActuators/SciSpark.java
@@ -102,18 +102,14 @@ public class SciSpark extends CANSparkMax implements RobotStateUpdater {
         double input = this.diminishSnap ? diminishSnap(limitedInput) : limitedInput;
         this.expectedVal = input;
         super.set(input);
-        
-        if (!Utils.inRange(input, super.get(), TOLERABLE_DIFFERENCE)) {
-            setWarning(input, super.get());
-        }
+        checkWarningStatus(input, super.get());
     }
 
-    public void setWarning(double expectedOutput, double realOutput){
-       // String warning = "WARNING: " + getDeviceName() + " was set to " + expectedOutput + " but still has a value of " + realOutput;
-        // System.out.println(warning);
-        // System.out.println(warning);
-        (new SparkDelayWarningCommand(this, expectedOutput)).start();
-        printDebuggingInfo();
+    public void checkWarningStatus(double input, double realOutput){
+        if (!Utils.inRange(input, super.get(), TOLERABLE_DIFFERENCE)) {
+            (new SparkDelayWarningCommand(this, input)).start();
+            printDebuggingInfo();        
+        }   
     }
 
     private double snapDecrementer(double x){
@@ -170,9 +166,6 @@ public class SciSpark extends CANSparkMax implements RobotStateUpdater {
         this.snapModel .updateRobotState();
         if(this.printValues){
             DelayedPrinter.print("Spark " + super.getDeviceId() + " value: " + super.get());
-        }
-        if(!Utils.impreciseEquals(this.expectedVal, super.get())){
-            // setWarning(this.expectedVal, super.get());
         }
     }
 
