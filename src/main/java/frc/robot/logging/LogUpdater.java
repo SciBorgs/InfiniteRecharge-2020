ab169234e8e0349f924836062e2e00203eec896d
zevbo <minsky-pz@bxscience.edu>
2020-02-14 14:41:14 -0500

    added default methods for automating stuffs

--- src/main/java/frc/robot/logging/LogUpdater.java
+++ src/main/java/frc/robot/logging/LogUpdater.java
@@ -1,7 +1,11 @@
 package frc.robot.logging;
 
+import frc.robot.Robot;
 
 public interface LogUpdater {
 
     public void periodicLog();
+    default void automateLogging(){
+        Robot.addLogUpdater(this);
+    }
 }
--- src/main/java/frc/robot/robotState/RobotStateUpdater.java
+++ src/main/java/frc/robot/robotState/RobotStateUpdater.java
@@ -1,6 +1,11 @@
 package frc.robot.robotState;
 
+import frc.robot.Robot;
+
 public interface RobotStateUpdater {
 
     public void updateRobotState();
+    default void enableUpdating(){
+        Robot.addRobotStateUpdater(this);
+    }
 }
